cfe529d1d98cbb34d4c29ac51b18b565f0466faa
tiagosilvahub <tiagodsilva93@gmail.com>
2019-12-13 21:35:46 +0100

    removed unnecessary ;

--- src/SolutionRunner.java
+++ src/SolutionRunner.java
@@ -9,7 +9,7 @@ public class SolutionRunner {
     public static void main(String[] args) {
         Solution[] solutions =
             new Solution[]{
-                new SlidingWindow(),
+                new SlidingWindow()
             };
 
         // add test cases
